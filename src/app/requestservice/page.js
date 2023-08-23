22cc8b1a8675a23e6fede6dfbbe6d4365f4a1cd6
darshg321 <darshg321@gmail.com>
2023-08-23 19:22:09 -0400

    pls

--- src/app/requestservice/page.js
+++ src/app/requestservice/page.js
@@ -3,7 +3,7 @@ import Posts from "@/(components)/posts";
 async function getPosts() {
     let jsonData
     try {
-        jsonData = await fetch(`${process.env.BASE_URL}/api/getposts?helptype=RequestHelp&limit=5`,
+        jsonData = await fetch(`${process.env.PREFIX}${process.env.VERCEL_URL}/api/getposts?helptype=RequestHelp&limit=5`,
             { cache: 'no-store' })
     }
     catch {
