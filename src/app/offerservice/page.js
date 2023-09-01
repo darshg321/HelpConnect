b1d933adbccd184d8e89091c44a5dd7a2693c1ee
darshg321 <darshg321@gmail.com>
2023-09-01 19:58:53 -0400

    something

--- src/app/offerservice/page.js
+++ src/app/offerservice/page.js
@@ -2,12 +2,7 @@ import Posts from "@/(components)/posts";
 
 async function getPosts() {
     let jsonData
-    if (process.env.VERCEL_URL) {
-        console.error(process.env.VERCEL_URL)
-    }
-    else {
-        console.error("me sad")
-    }
+
     try {
         jsonData = await fetch(`${process.env.PREFIX}${process.env.VERCEL_URL}/api/getposts?helptype=OfferHelp&limit=5`,
             { cache: 'no-store' })
